c639418b3924cd34822c542408d42846216cce22
Jessica Dao <xjessicadao@gmail.com>
2021-02-16 18:12:21 -0800

    plural/singular fixes, changed to port 3000 for preference

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,4 +1,4 @@
-INSERT INTO burgers (burger_name) VALUES ("");
-INSERT INTO burgers (burger_name) VALUES ("");
-INSERT INTO burgers (burger_name) VALUES ("");
+INSERT INTO burgers (burger_name) VALUES ("hamburger");
+INSERT INTO burgers (burger_name) VALUES ("cheeseburger");
+INSERT INTO burgers (burger_name) VALUES ("veggie burger");
 
